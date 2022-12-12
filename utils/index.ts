6e3dd67a5581c19399438e6557397920c38da7b7
KevinAngeles <KevinAngeles@users.noreply.github.com>
2022-12-11 23:43:19 -0500

    Fix bug to parse worksheet name according to Excel requirements.

--- utils/index.ts
+++ utils/index.ts
@@ -2,3 +2,4 @@ export { default as createEmotionCache } from './createEmotionCache';
 export { getAccessAndRefreshToken, refreshSpotifyToken } from './tokenHandler';
 export { verifySpotifyUserExists, createSpotifyUserExistsJSON, updateAccountToken } from './spotifyUserHandler';
 export { getErrorMessage } from './errorHandler';
+export { parseStringToUTF8, parseWorksheetName } from './stringHandler';
